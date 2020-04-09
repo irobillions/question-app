c797863d54aeffdf1a86c407cb3494e09abbf415
irobillions <christbouka14@yahoo.fr>
2020-04-09 20:07:02 +0100

    database helpers for connect and get db

--- src/app/database/database.py
+++ src/app/database/database.py
@@ -1,14 +1,33 @@
 from flask import g
-import sqlite3
+import psycopg2
+from psycopg2.extras import DictCursor
 
 
 def connect_db():
-    sql = sqlite3.connect('C:\\Users\\hp\\Travaux\\database\\questionapp.db')
-    sql.row_factory = sqlite3.Row
-    return sql
+    conn = psycopg2.connect(
+        'postgres://bgbnqaqewclesp:b2395d57ab343efaa44bbc00ac44708f58d1bc0c9d4d6dc6aac488e880d6dfbe@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/dd48fd2e249r67',
+        cursor_factory=DictCursor)
+    conn.autocommit = True
+    sql = conn.cursor()
+    return conn, sql
 
 
 def get_db():
-    if not hasattr(g, 'sqlite3'):
-        g.sqlite_db = connect_db()
-        return g.sqlite_db
+    db = connect_db()
+
+    if not hasattr(g, 'postgres_db_conn'):
+        g.postgres_db_conn = db[0]
+
+    if not hasattr(g, 'psotgres_db_cur'):
+        g.postgres_db_cur = db[1]
+
+    return g.postgres_db_cur
+
+
+def init_db():
+    db = connect_db()
+    db[1].execute(open('schema.sql', 'r').read())
+    db[1].close()
+
+    db[0].close()
+
